9f10f218cdd9c732b168fe4a103f254910d291db
Guga Villar <grsv.21@gmail.com>
2023-11-26 21:50:36 -0300

    :lipstick: Adjust in portfolio page

--- src/slices/Customer/index.tsx
+++ src/slices/Customer/index.tsx
@@ -11,16 +11,16 @@ const Customer = ({ slice }: CustomerProps): JSX.Element => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className='w-full px-6 md:px-16'
+      className='w-full border-y-2 border-portfolio-gray-500 bg-portfolio-gray-700 bg-opacity-50 p-6 md:px-16'
     >
       <Subtitle subtitle='Clientes' />
-      <ul className='flex w-full gap-8 pl-4 md:pl-16'>
+      <ul className='flex w-full items-center gap-8 px-4 md:px-16'>
         {slice.items.map((item, index) => (
-          <li key={index} className='w-32 space-y-4 py-6'>
-            <PrismicNextImage field={item.customer_logo} />
-            <p className='text-center text-white opacity-50'>
-              {item.customer_name}
-            </p>
+          <li key={index} className='flex space-y-4'>
+            <PrismicNextImage
+              className='w-48 object-contain p-3'
+              field={item.customer_logo}
+            />
           </li>
         ))}
       </ul>
